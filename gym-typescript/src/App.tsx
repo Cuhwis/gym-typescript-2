7e0173831fff011772f632a5f300c42fc6922281
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-27 21:16:40 -0700

    progress with navbar

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,13 +1,23 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClassese = "ourclasses",
+  ContactUs = "contactus",
+}
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>(SelectedPage.Home);
 
   return (
     <>
-      <div className='app'>app
+      <div className="app bg-gray-20">
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
