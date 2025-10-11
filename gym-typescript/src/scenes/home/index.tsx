1bbedf963023b984616fe2465f776f4a1e99a8a9
ChrisRuaboro <chrisruaboro@gmail.com>
2025-10-10 17:46:41 -0700

    added home function component. was deleted

--- gym-typescript/src/scenes/home/index.tsx
+++ gym-typescript/src/scenes/home/index.tsx
@@ -2,6 +2,7 @@ import type { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import HomePageText from "@/assets/HomePageText.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import Sponsorforbes from "@/assets/Sponsorforbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
@@ -9,9 +10,29 @@ import SponsorFortune from "@/assets/SponsorFortune.png";
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
-
-const isAboveMediumScreens = useMediaQuery("(min-width:1060px")
-  return <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pd-0">Home</section>;
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px");
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      Home
+      {/* IMAGE AND MAIN HEADER */}
+      <div className="">
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
