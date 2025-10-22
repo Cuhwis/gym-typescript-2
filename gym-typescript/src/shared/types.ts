9b0fc68b997294119da5157feea046f9b37934a7
ChrisRuaboro <chrisruaboro@gmail.com>
2025-10-22 13:06:37 -0700

    benefits page component add

--- gym-typescript/src/shared/types.ts
+++ gym-typescript/src/shared/types.ts
@@ -1,6 +1,14 @@
+import type { ReactElement } from "react";
+
 export enum SelectedPage {
   Home = "home",
   Benefits = "benefits",
   OurClassese = "ourclasses",
   ContactUs = "contactus",
+}
+
+export interface BenefitType {
+  icon: ReactElement;
+  title: string;
+  description: string;
 }
