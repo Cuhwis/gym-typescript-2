e8c8761679152fa5099382c9e36e073d1da13bd3
ChrisRuaboro <chrisruaboro@gmail.com>
2025-11-06 15:29:13 -0800

    Contact Us edits

--- gym-typescript/src/scenes/contactUs/index.tsx
+++ gym-typescript/src/scenes/contactUs/index.tsx
@@ -1,7 +1,20 @@
-type Props = {};
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactPageUsGraphic from "@/assets/ContactUsPageGraphic.png";
 
-const ContactUs = (props: Props) => {
-  return <div>ContactUs</div>;
+type Props = {
+  setselectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      ></motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
